1c6c32a1818e1da5cbdbc57d27b79cd1ecb2b333
tibebit <fabiotibe.bit@gmail.com>
2021-02-27 15:17:12 +0100

    Initial Commit

---
+++ Meme1.0-2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Meme1.0-2
+//
+//  Created by Fabio Tiberio on 27/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Meme1.0-2Tests/Meme1_0_2Tests.swift
@@ -0,0 +1,33 @@
+//
+//  Meme1_0_2Tests.swift
+//  Meme1.0-2Tests
+//
+//  Created by Fabio Tiberio on 27/02/21.
+//
+
+import XCTest
+@testable import Meme1_0_2
+
+class Meme1_0_2Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
