ae39b08cb56698bda08e95b25135a8af2eb67d0d
tibebit <fabiotibe.bit@gmail.com>
2021-03-02 16:00:55 +0100

    keyboard will hide notification subscribing/unsubscribing code added

--- Meme1.0-2/ViewController.swift
+++ Meme1.0-2/ViewController.swift
@@ -45,15 +45,21 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     func subscribeToKeyboardNotifications() {
         
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
     
     func unsubscribeFromKeyboardNotifications() {
 
         NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
     }
     //MARK: Selector Methods
     @objc func keyboardWillShow(_ notification:Notification) {
         view.frame.origin.y -= getKeyboardHeight( notification)
+    }
+    
+    @objc func keyboardWillHide(_ notification: Notification) {
+        view.frame.origin.y = 0.0
     }
     //MARK: Keyboard
     func getKeyboardHeight(_ notification: Notification) -> CGFloat {
