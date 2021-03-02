1f69918c3493228e168f0e99234a256d2114285c
tibebit <fabiotibe.bit@gmail.com>
2021-03-02 13:21:02 +0100

    Image outlet added

--- Meme1.0-2/ViewController.swift
+++ Meme1.0-2/ViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imagePickerView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
