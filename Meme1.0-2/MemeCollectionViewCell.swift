9927afdce9e9196fc5bf58237e8f49fb68f2d68f
tibebit <fabiotibe.bit@gmail.com>
2021-03-21 22:30:54 +0100

    file added to project

---
+++ Meme1.0-2/MemeCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  MemeCollectionViewCell.swift
+//  Meme1.0-2
+//
+//  Created by Fabio Tiberio on 21/03/21.
+//
+
+import UIKit
+
+class MemeCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var memedImage: UIImageView!
+}
