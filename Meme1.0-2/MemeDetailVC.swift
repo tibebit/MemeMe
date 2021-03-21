d2cf91f8f246e65f813e3c47d25dbbf11b8dccf0
tibebit <fabiotibe.bit@gmail.com>
2021-03-21 19:54:29 +0100

    file added to project

---
+++ Meme1.0-2/MemeDetailVC.swift
@@ -0,0 +1,14 @@
+//
+//  MemeDetailVC.swift
+//  Meme1.0-2
+//
+//  Created by Fabio Tiberio on 21/03/21.
+//
+
+import UIKit
+
+class MemeDetailVC: UIViewController {
+    
+    @IBOutlet weak var memedImage: UIImageView!
+    
+}
