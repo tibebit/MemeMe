07d7cbfbd3782ae553466d81535e3df87e558b40
tibebit <fabiotibe.bit@gmail.com>
2021-03-21 19:08:04 +0100

    sent memes collection view controller refactored

--- Meme1.0-2/SentMemesCollectionVC.swift
+++ Meme1.0-2/SentMemesCollectionVC.swift
@@ -1,5 +1,5 @@
 //
-//  SentMemesViewController.swift
+//  SentMemesCollectionVC.swift
 //  Meme1.0-2
 //
 //  Created by Fabio Tiberio on 16/03/21.
@@ -7,16 +7,13 @@
 
 import UIKit
 
-class SentMemesViewController: UIViewController {
+class SentMemesCollectionVC: UIViewController {
     
     //MARK: Properties
-    var memes: [Meme]! {
+    var memes: [Meme] {
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
         return appDelegate.memes
     }
-    
-    var tableView: UITableView!
-    var collectionView: UICollectionView!
     
     override func viewDidLoad() {
         
