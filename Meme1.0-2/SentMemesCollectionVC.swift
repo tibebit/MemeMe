df46791cd1484652fd47afa7525232ba779d1537
tibebit <fabiotibe.bit@gmail.com>
2021-03-21 20:25:21 +0100

    sent memes view controller refactored

--- Meme1.0-2/SentMemesCollectionVC.swift
+++ Meme1.0-2/SentMemesCollectionVC.swift
@@ -2,29 +2,28 @@
 //  SentMemesCollectionVC.swift
 //  Meme1.0-2
 //
-//  Created by Fabio Tiberio on 16/03/21.
+//  Created by Fabio Tiberio on 21/03/21.
 //
 
 import UIKit
 
 class SentMemesCollectionVC: UICollectionViewController {
-    
-    //MARK: Properties
     var memes: [Meme] {
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
         return appDelegate.memes
     }
-    
     //MARK: Data Source
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return memes.count
     }
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let item = collectionView.dequeueReusableCell(withReuseIdentifier identifier: "MemeItem", for indexPath: indexPath)
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let item = collectionView.dequeueReusableCell(withReuseIdentifier: "MemeItem", for: indexPath)
         return item
     }
     //MARK: Delegate
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        let detailVC
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let memeDetailVC = storyboard?.instantiateViewController(withIdentifier: "MemeDetailVC") as! MemeDetailVC
+        memeDetailVC.memedImage.image = memes[indexPath.row].memedImage
+        navigationController?.pushViewController(memeDetailVC, animated: true)
     }
 }
