e378d00398da7faeff8f96b3c4285a072cca8988
tibebit <fabiotibe.bit@gmail.com>
2022-05-12 11:22:42 +0200

    feat: add custom text field class for the Meme's caption

---
+++ Meme1.0-2/MemeMeTextField.swift
@@ -0,0 +1,31 @@
+//
+//  MemeMeTextField.swift
+//  Meme1.0-2
+//
+//  Created by Fabio Tiberio on 12/05/22.
+//
+
+import UIKit
+
+public class MemeMeTextField: UITextField {
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        configure()
+    }
+
+    private func configure() {
+        defaultTextAttributes = [
+            .strokeColor: UIColor.black,
+            .foregroundColor: UIColor.white,
+            .font: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+            .strokeWidth: -3.0
+        ]
+       textAlignment = .center
+       adjustsFontSizeToFitWidth = true
+       clearsOnBeginEditing = true
+    }
+    
+    public func set(text: String) {
+        self.text = text
+    }
+}
