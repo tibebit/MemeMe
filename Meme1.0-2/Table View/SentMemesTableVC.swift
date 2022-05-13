b2f8112a83964e72ca18838fe887d48cce98f90b
tibebit <fabiotibe.bit@gmail.com>
2022-05-13 18:59:01 +0200

    feat: use the meme view model for the table view configuration

--- Meme1.0-2/Table View/SentMemesTableVC.swift
+++ Meme1.0-2/Table View/SentMemesTableVC.swift
@@ -8,39 +8,30 @@
 import UIKit
 
 class SentMemesTableVC: UITableViewController {
-    
-    //MARK: Properties
-    var memes: [Meme]! {
-        let delegate = UIApplication.shared.delegate as! AppDelegate
-        return delegate.memes
-    }
-    
+    var memeCollectionViewModel = MemeCollectionViewModel()
+
     //MARK: Lifecycle
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         tableView.reloadData()
     }
-    
-    //MARK: Data Source
+}
+
+//MARK: - Data Source
+extension SentMemesTableVC {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return memes.count
+        return memeCollectionViewModel.memes.count
     }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "MemeTableCell") as! MemeTableViewCell
-        
-        let meme = getCurrentMeme(memes: memes, indexPath: indexPath)
-        cell.memedImage?.image = meme.memedImage
-        cell.memeText?.text = meme.topText + " " + meme.bottomText
-        return cell
+        let cell = tableView.dequeueReusableCell(withIdentifier: MemeTableViewCell.identifier) as! MemeTableViewCell
+        return memeCollectionViewModel.configure(cell: cell, at: indexPath)
     }
-    //MARK: Delegate
+}
+//MARK: - Delegate
+extension SentMemesTableVC {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let memeDetailVC = storyboard?.instantiateViewController(withIdentifier: "MemeDetailVC") as! MemeDetailVC
-        memeDetailVC.meme = getCurrentMeme(memes: memes, indexPath: indexPath)
+        memeDetailVC.meme = memeCollectionViewModel.getCurrentMeme(at: indexPath)
         navigationController?.pushViewController(memeDetailVC, animated: true)
-    }
-    //MARK: Utility
-    func getCurrentMeme(memes: [Meme], indexPath: IndexPath) -> Meme {
-        return memes[indexPath.row]
     }
 }
