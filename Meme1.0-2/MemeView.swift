521a287b27b70968b23bfe4d0a5831d6c06d15c7
tibebit <fabiotibe.bit@gmail.com>
2022-05-13 21:48:24 +0200

    feat: add meme view class

---
+++ Meme1.0-2/MemeView.swift
@@ -0,0 +1,15 @@
+//
+//  MemeView.swift
+//  Meme1.0-2
+//
+//  Created by Fabio Tiberio on 13/05/22.
+//
+
+import UIKit
+
+class MemeView: UIView {
+    
+    @IBOutlet weak var imagePickerView: UIImageView!
+    @IBOutlet weak var topTextField: MemeMeTextField!
+    @IBOutlet weak var bottomTextField: MemeMeTextField!
+}
